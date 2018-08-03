08f679356020d04ca0e0e81ccbc680efb299a9d8
justinwoo <moomoowoo@gmail.com>
2018-08-03 10:25:56 +0200

    add purs-install.bash

---
+++ purs-install.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TAG=v0.12.0
+PSC_PACKAGE_TAG=v0.4.1
+
+wget -O $HOME/purescript.tar.gz https://github.com/purescript/purescript/releases/download/$TAG/linux64.tar.gz
+tar -xvf $HOME/purescript.tar.gz -C $HOME/
+mv $HOME/purescript/purs ~/.local/bin
+
+wget -O $HOME/psc-package.tar.gz https://github.com/purescript/psc-package/releases/download/$PSC_PACKAGE_TAG/linux64.tar.gz
+tar -xvf $HOME/psc-package.tar.gz -C $HOME/
+mv $HOME/psc-package/psc-package ~/.local/bin
